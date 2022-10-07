985f4f72beefeb1ecea4da31a0ceb4ba667061e3
Reggie Williams <rlwjr433@gmail.com>
2022-10-07 10:44:33 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const cup of clay) {
+   const mug = `${cup} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln) 
